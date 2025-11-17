be4f29524190bbd7d864568ffaa7b94bf1be6011
Adam Foerster <54410227+adam-foerster@users.noreply.github.com>
2025-11-16 18:01:39 -0700

    Update Vite configuration for improved build settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+	base: "/StakeWebsite/", // Should match your repo name
+	build: {
+		outDir: "dist",
+	},
 	plugins: [tailwindcss()],
 });
