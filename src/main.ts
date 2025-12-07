f360f03a6ace4d8124f363087044c200ea29ca8a
Adam Foerster <54410227+adam-foerster@users.noreply.github.com>
2025-12-07 14:50:27 -0700

    Add padding to main section and embed Google Calendar and Maps iframes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ let footer = setupFooter();
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
 ${header.outerHTML}
-<main class="flex flex-col items-center justify-center gap-4">
+<main class="flex flex-col items-center justify-center gap-4 pb-4">
   <section class="heroSlider w-full relative">
     <div class="swiper max-h-96" style="clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%)">
         <div class="swiper-wrapper relative">
@@ -35,22 +35,17 @@ ${header.outerHTML}
 
   </section>
 
-  <section class="wardDiscovery max-w-7xl mx-auto">
-    <h2 class="text-3xl font-bold mb-4">Discover Your Ward</h2>
-  </section>
-
   <section class="upcomingEvents max-w-7xl mx-auto">
     <h2 class="text-3xl font-bold mb-4">Upcoming Events</h2>
-  </section>
-
-  <section class="testimonials">
-    <h2 class="text-3xl font-bold mb-4">Testimonials</h2>
+    <iframe src="https://calendar.google.com/calendar/embed?src=87d84b26256aab9ebb2412356659beb07f21202cfc1ae9335b1408c5a1349f03%40group.calendar.google.com&ctz=America%2FDenver" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
   </section>
 
   <section class="stakeCenter">
     <h2 class="text-3xl font-bold mb-4">Stake Center Location</h2>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d757.9571842925571!2d-111.86366440882615!3d40.54537272639968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875287007cf569c5%3A0x8d6718edbc63d28b!2sDraper%20YSA%20Stake%20Center!5e0!3m2!1sen!2sus!4v1765143414932!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </section>
 </main>
+
  ${footer.outerHTML}
 `;
 
