e63cf608dae33a3732e8b06bdee74b1812cb438d
Adam Foerster <54410227+adam-foerster@users.noreply.github.com>
2025-12-07 15:15:19 -0700

    Updated the ward page, and gathering place page to be build by the vite builder.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
 	base: "/StakeWebsite/", // Should match your repo name
 	build: {
 		outDir: "dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				ward: resolve(__dirname, "ward.html"),
+				gatheringPlace: resolve(__dirname, "gatheringPlace.html"),
+			},
+		},
 	},
 	plugins: [tailwindcss()],
 });
