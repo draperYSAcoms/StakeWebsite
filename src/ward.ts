a6f001be2b3f9c63c2f1ac7666e35645d8b04167
Adam Foerster <54410227+adam-foerster@users.noreply.github.com>
2025-12-21 16:54:53 -0700

    Refactor ward data structure to use objects for feeder wards; update related logic and HTML generation for improved clarity and maintainability.

--- src/ward.ts
+++ src/ward.ts
@@ -60,138 +60,338 @@ const wards = [
 
 //Feeder Wards Data
 const feederWards = [
-	"Corner Canyon 1st Ward",
-	"Corner Canyon 2nd Ward",
-	"Corner Canyon 3rd Ward",
-	"Corner Canyon 4th Ward",
-	"Corner Canyon 5th Ward",
-	"Corner Canyon 6th Ward",
-	"Corner Canyon 7th Ward",
-	"Corner Canyon 8th Ward",
-	"Corner Canyon 9th Ward",
-	"South Mountain 1st Ward",
-	"South Mountain 2nd Ward",
-	"South Mountain 3rd Ward",
-	"South Mountain 4th Ward",
-	"South Mountain 5th Ward",
-	"South Mountain 6th Ward",
-	"South Mountain 7th Ward",
-	"South Mountain 8th Ward",
-	"Eagle Crest 1st Ward",
-	"Eagle Crest 2nd Ward",
-	"Eagle Crest 3rd Ward",
-	"Eagle Crest 4th Ward",
-	"Suncrest 1st Ward",
-	"Suncrest 2nd Ward",
-	"Suncrest 3rd Ward",
-	"Meadows 2nd Ward",
-	"Meadows 5th Ward",
-	"Meadows 7th Ward",
-	"Draper 1st Ward",
-	"Draper 2nd Ward",
-	"Draper 3rd Ward",
-	"Draper 4th Ward",
-	"Draper 5th Ward",
-	"Draper 6th Ward",
-	"Draper 7th Ward",
-	"Draper 8th Ward",
-	"Draper 9th Ward",
-	"Draper 10th Ward",
-	"Draper 11th Ward",
-	"Draper 12th Ward",
-	"Draper 15th Ward",
-	"Draper 16th Ward",
-	"Draper 17th Ward",
-	"Riverview 8th Ward",
-	"Riverview 10th Ward",
-	"Eastridge 1st Ward",
-	"Eastridge 2nd Ward",
-	"Eastridge 3rd Ward",
-	"Eastridge 4th Ward",
-	"Eastridge 5th Ward",
-	"Eastridge 6th Ward",
-	"Eastridge 7th Ward",
-	"Eastridge 8th Ward",
-	"Eastridge 9th Ward (Spanish)",
-	"Crescent 6th Ward",
-	"Crescent 9th Ward",
-	"Crescent 10th Ward",
-	"Crescent 11th Ward",
-	"Crescent 12th Ward",
-	"Crescent 14th Ward",
-	"Crescent 16th Ward",
-	"Crescent 18th Ward",
-	"Crescent 22nd Ward",
-	"Crescent 23rd Ward",
-	"Crescent 24th Ward",
-	"Crescent 25th Ward",
-	"Crescent 28th Ward",
-	"Crescent 30th Ward",
-	"Crescent 1st Ward (Spanish)",
-	"Crescent View Ward",
-	"Crescent Ridge 2nd Ward",
-	"Crescent Ridge 3rd Ward",
-	"Crescent Ridge 4th Ward",
-	"Crescent Ridge 5th Ward",
-	"Crescent Ridge 6th Ward",
-	"Crescent Ridge 7th Ward",
-	"River View 1st Ward",
-	"River View 2nd Ward",
-	"River View 3rd Ward",
-	"River View 4th Ward",
-	"River View 5th Ward",
-	"River View 9th Ward",
-	"Hidden Valley 4th Ward",
-	"Alta Ward",
-	"Amaryllis Ward",
-	"Larkspur Ward",
-	"Mountain Peak Ward (Portuguese)",
-	"Sandy Creek Ward",
-	"Savannah Ward",
-	"Windy Peak Ward",
-	"Buttercup Ward",
-	"Glendover Ward",
-	"Heytesbury Ward",
-	"Wildflower Ward",
-	"Ashley Park Ward",
-	"Eastdell Ward",
-	"Falcon Hill Ward",
-	"Karalee Ward",
-	"Parkview Ward",
-	"Sego Lily Ward",
-	"Dimple Dell Ward",
-	"Dimple Dell Park Ward",
-	"Lone Hollow Ward",
-	"Northridge Ward",
-	"Wanderwood Ward",
-	"Bell Canyon Ward",
-	"Glacial Park Ward",
-	"Granite Ward",
-	"Granite Ridge Ward",
-	"Granite View Ward",
-	"Little Cottonwood Ward",
-	"Hegessy Ward",
-	"Legacy Park Ward",
-	"Meadowlark Ward",
-	"Newcastle Ward",
-	"Quail Hollow Ward",
-	"Willow Hills Ward",
-	"Willow Creek 1st Ward",
-	"Willow Creek 2nd Ward",
-	"Willow Creek 3rd Ward",
-	"Willow Creek 4th Ward",
-	"Willow Creek 6th Ward",
-	"Willow Creek 7th Ward",
-	"Willow Creek 9th Ward",
-	"Lone Peak 1st Ward",
-	"Lone Peak 2nd Ward",
-	"Lone Peak 3rd Ward",
-	"Lone Peak 4th Ward",
-	"Lone Peak 5th Ward",
-	"Albion Ward",
-	"Indian Ridge Ward",
-	"Aspen Hills Ward",
-	"Highland Ward",
+	{
+		name: "Corner Canyon 1st Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 2nd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 3rd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 4th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 5th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 6th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 7th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 8th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Corner Canyon 9th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 1st Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 2nd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 3rd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 4th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 5th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 6th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 7th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "South Mountain 8th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Eagle Crest 1st Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Eagle Crest 2nd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Eagle Crest 3rd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Eagle Crest 4th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Suncrest 1st Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Suncrest 2nd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Suncrest 3rd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Meadows 2nd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Meadows 5th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Meadows 7th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Draper 1st Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Draper 2nd Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Draper 3rd Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Draper 4th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Draper 5th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Draper 6th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Draper 7th Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Draper 8th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Draper 9th Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Draper 10th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Draper 11th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Draper 12th Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Draper 15th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Draper 16th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Draper 17th Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Riverview 8th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{
+		name: "Riverview 10th Ward",
+		younger: "South Mountain",
+		older: "Willow Springs",
+	},
+	{ name: "Eastridge 1st Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 2nd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 3rd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 4th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 5th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 6th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 7th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastridge 8th Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Eastridge 9th Ward (Spanish)",
+		younger: "Alta",
+		older: "Crescent Park",
+	},
+	{ name: "Crescent 6th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 9th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 10th Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Crescent 11th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 12th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 14th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 16th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 18th Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Crescent 22nd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 23rd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 24th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 25th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 28th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent 30th Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Crescent 1st Ward (Spanish)",
+		younger: "Alta",
+		older: "Crescent Park",
+	},
+	{ name: "Crescent View Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Crescent Ridge 2nd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent Ridge 3rd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent Ridge 4th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent Ridge 5th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent Ridge 6th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Crescent Ridge 7th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "River View 1st Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "River View 2nd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "River View 3rd Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "River View 4th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "River View 5th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "River View 9th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Hidden Valley 4th Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Alta Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Amaryllis Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Larkspur Ward", younger: "Alta", older: "Crescent Park" },
+	{
+		name: "Mountain Peak Ward (Portuguese)",
+		younger: "Alta",
+		older: "Crescent Park",
+	},
+	{ name: "Sandy Creek Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Savannah Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Windy Peak Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Buttercup Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Glendover Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Heytesbury Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Wildflower Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Ashley Park Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Eastdell Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Falcon Hill Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Karalee Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Parkview Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Sego Lily Ward", younger: "Alta", older: "Crescent Park" },
+	{ name: "Dimple Dell Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{
+		name: "Dimple Dell Park Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{ name: "Lone Hollow Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Northridge Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Wanderwood Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Bell Canyon Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Glacial Park Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Granite Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Granite Ridge Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Granite View Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{
+		name: "Little Cottonwood Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{ name: "Hegessy Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Legacy Park Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Meadowlark Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Newcastle Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Quail Hollow Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Willow Hills Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{
+		name: "Willow Creek 1st Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{
+		name: "Willow Creek 2nd Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{
+		name: "Willow Creek 3rd Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{
+		name: "Willow Creek 4th Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{
+		name: "Willow Creek 6th Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{
+		name: "Willow Creek 7th Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{
+		name: "Willow Creek 9th Ward",
+		younger: "Lone Peak",
+		older: "Willow Creek",
+	},
+	{ name: "Lone Peak 1st Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Lone Peak 2nd Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Lone Peak 3rd Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Lone Peak 4th Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Lone Peak 5th Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Albion Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Indian Ridge Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Aspen Hills Ward", younger: "Lone Peak", older: "Willow Creek" },
+	{ name: "Highland Ward", younger: "Lone Peak", older: "Willow Creek" },
 ];
 
 // Generate ward cards HTML
@@ -226,20 +426,56 @@ const wardCards = wards
 
 // Generate feeder wards options HTML
 const feederWardOptions = feederWards
-	.map((ward) => `<option value="${ward}">${ward}</option>`)
+	.map((ward) => `<option value="${ward.name}">${ward.name}</option>`)
 	.join("");
 
 // Calculate YSA Ward based on feeder ward and age group (placeholder logic)
-// function calculateYsaWard(feederWard: string, ageGroup: string): string {
-// 	// Placeholder logic for demonstration purposes only
-// 	if (ageGroup === "18-25") {
-// 		return "Alta Ward 18-25";
-// 	} else if (ageGroup === "26-35") {
-// 		return "Crescent Park Ward 26-35";
-// 	} else {
-// 		return "Please select a valid age group.";
-// 	}
-// }
+function calculateYsaWard(feederWard: string, ageGroup: string): string {
+	// Placeholder logic for demonstration purposes only
+	if (ageGroup === "18-25") {
+		return "Alta Ward 18-25";
+	} else if (ageGroup === "26-35") {
+		return "Crescent Park Ward 26-35";
+	} else {
+		return "Please select a valid age group.";
+	}
+}
+
+function rendorResult() {
+	const resultDiv = document.getElementById("result")!;
+	const feederWardSelect = document.querySelector("select")!;
+	const ageGroupButtons = document.getElementsByName(
+		"ageGroup"
+	) as NodeListOf<HTMLButtonElement>;
+
+	const feederWard = feederWardSelect.value;
+	let ageGroup = "";
+	ageGroupButtons.forEach((button) => {
+		if (button.classList.contains("selected")) {
+			ageGroup = button.value;
+		}
+	});
+
+	const ysaWard = calculateYsaWard(feederWard, ageGroup);
+	resultDiv.textContent = `Your YSA Ward is: ${ysaWard}`;
+}
+
+// Add event listener to "Find My Ward" button
+document.addEventListener("DOMContentLoaded", () => {
+	const findWardButton = document.querySelector("button.bg-cyan-500")!;
+	findWardButton.addEventListener("click", rendorResult);
+
+	// Add toggle functionality for age group buttons
+	const ageGroupButtons = document.getElementsByName(
+		"ageGroup"
+	) as NodeListOf<HTMLButtonElement>;
+	ageGroupButtons.forEach((button) => {
+		button.addEventListener("click", () => {
+			ageGroupButtons.forEach((btn) => btn.classList.remove("selected"));
+			button.classList.add("selected");
+		});
+	});
+});
 
 // Create main content
 const main = document.createElement("main");
@@ -249,27 +485,26 @@ main.innerHTML = `
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
 		${wardCards}
 	</div>
-<!--
+
     <h2 class="text-2xl font-bold text-center mt-10">Find Your YSA Ward</h2>
     <div class="text-center text-sm text-gray-500 mt-6">
         <p>Search your home (feeder) ward or stake. Choose your age group and we’ll point you to the right YSA ward.</p>
        
         <p class="block mt-4">Feeder Ward:</p>
-         <Select class="mt-4 p-2 border border-gray-300 rounded">
+         <select class="mt-4 p-2 border border-gray-300 rounded">
             <option value="" disabled selected>Select your feeder ward</option>
             ${feederWardOptions}
-        </Select>
-
+        </select>
         <p class="block mt-4">Age Group:</p>
-        <button type="radio" name="ageGroup" value="18-25" class="mx-2 px-4 py-2 border border-gray-300 rounded hover:bg-gray-100">18-25</button>
-        <button type="radio" name="ageGroup" value="26-35" class="mx-2 px-4 py-2 border border-gray-300 rounded hover:bg-gray-100">26-35</button>
+        <input type="radio" name="ageGroup" value="18-25" class="mx-2 px-4 py-2 border border-gray-300 rounded hover:bg-gray-100 checked:bg-gray-100">18-25
+        <input type="radio" name="ageGroup" value="26-35" class="mx-2 px-4 py-2 border border-gray-300 rounded hover:bg-gray-100 checked:bg-gray-100">26-35
 
         <div class="mt-6"> 
             <button class="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 rounded transition-colors">Find My Ward</button>
         </div>
         <div id="result" class="mt-4 text-lg font-semibold"></div>
      </div>
-     -->
+
 `;
 
 // Append header, main, and footer to app
