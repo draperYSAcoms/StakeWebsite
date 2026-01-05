97251dbf6a004e3719f244c07dbff11a621dd92f
Adam Foerster <54410227+adam-foerster@users.noreply.github.com>
2026-01-04 20:54:42 -0700

    Update Vite configuration to set base path to root for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,23 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
+// export default defineConfig({
+// 	base: "/StakeWebsite/", // Should match your repo name
+// 	build: {
+// 		outDir: "dist",
+// 		rollupOptions: {
+// 			input: {
+// 				main: resolve(__dirname, "index.html"),
+// 				ward: resolve(__dirname, "ward.html"),
+// 				gatheringPlace: resolve(__dirname, "gatheringPlace.html"),
+// 			},
+// 		},
+// 	},
+// 	plugins: [tailwindcss()],
+// });
+
 export default defineConfig({
-	base: "/StakeWebsite/", // Should match your repo name
+	base: "/",
 	build: {
 		outDir: "dist",
 		rollupOptions: {
